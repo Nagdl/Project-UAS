6f5d5bc943470e7808a524c40ab4fa3904ea27b7
Nagdl <137997774+Nagdl@users.noreply.github.com>
2024-05-06 16:18:04 +0700

    Tambah IsiDua

--- src/components/Home/IsiDua.jsx
+++ src/components/Home/IsiDua.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./IsiDua.css";
+import LampungDesc from "../../assets/LampungDesc.jpg"
 
 function IsiDua() {
     return(
         <div className="containerIsiDua">
-            <div className="Lampung">
-
+            <div className="Judul">
+                <p>LAMPUNG's DESCRIPTION</p>
             </div>
-            <div className="text">
-                
+
+            <div className="desc2">
+                <div className="Lampung">
+                    <img src={LampungDesc} />
+                </div>
+                <div className="Text">
+                    <p>Lampung adalah sebuah provinsi di bagian ujung selatan Pulau Sumatra, Indonesia. Ibu kota provinsi ini terletak di Kota Bandar Lampung.[11] Provinsi ini memiliki dua kota, yaitu Kota Bandar Lampung dan Kota Metro, serta 13 kabupaten. Secara geografis, posisi provinsi ini di sebelah barat berbatasan dengan Samudra Hindia, di sebelah timur dengan Laut Jawa, di sebelah utara berbatasan dengan Provinsi Sumatera Selatan dan Provinsi Bengkulu, serta di sebelah selatan berbatasan dengan Selat Sunda.</p>
+                </div>
             </div>
         </div>
     );

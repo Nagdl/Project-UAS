119811946369fcb842f6180e7e87ed4fd4982b31
Grefred <163652991+Grefred@users.noreply.github.com>
2024-05-30 20:20:21 +0700

    tes navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,33 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import "./Navbar.css";
 import logo from "../../assets/SigerLampung.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function Navbar(){
-    const [containerNavbar, setNavbar] = useState(false); 
-    const [ bungkusLogo , setLogo ] = useState(false);
+function Navbar() {
+    const [containerNavbar, setNavbar] = useState(false);
+    const [bungkusLogo, setLogo] = useState(false);
+    const location = useLocation();
 
     useEffect(() => {
         const changeBackground = () => {
-            console.log(window.scrollY)
-            if(window.scrollY >= 100){
+            if (window.scrollY >= 100) {
                 setNavbar(true);
-                setLogo(true)
+                setLogo(true);
             } else {
                 setNavbar(false);
                 setLogo(false);
-            } 
+            }
         };
 
         window.addEventListener('scroll', changeBackground);
+
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        };
     }, []);
-    
-    return(
-        <nav className= {containerNavbar ? "containerNavbar aktif navbar navbar-expand-lg navbar-dark fixed-top" : "containerNavbar navbar navbar-expand-lg navbar-dark fixed-top p-4"}>
+
+    useEffect(() => {
+        // Close the off-canvas sidebar when the route changes
+        const sidebar = document.getElementById('offcanvasNavbar');
+        if (sidebar) {
+            const bsOffcanvas = new window.bootstrap.Offcanvas(sidebar);
+            bsOffcanvas.hide();
+        }
+
+        // Reset the body's overflow style
+        document.body.style.overflow = 'auto';
+    }, [location]);
+
+    const closeSidebar = () => {
+        const sidebar = document.getElementById('offcanvasNavbar');
+        if (sidebar) {
+            const bsOffcanvas = new window.bootstrap.Offcanvas(sidebar);
+            bsOffcanvas.hide();
+        }
+    };
+
+    return (
+        <nav className={containerNavbar ? "containerNavbar aktif navbar navbar-expand-lg navbar-dark fixed-top" : "containerNavbar navbar navbar-expand-lg navbar-dark fixed-top p-4"}>
             <div className="container-fluid">
                 {/* Logo */}
                 <div className={bungkusLogo ? "aktifLogo" : "bungkusLogo"}>
-                    <div className="iconLogo"><img src={logo} alt=""/></div>
+                    <div className="iconLogo"><img src={logo} alt="Logo" /></div>
                     <div className="teksLogo"><p>LAMPCARA</p></div>
                 </div>
                 {/* Toggle Button */}
@@ -36,10 +60,10 @@ function Navbar(){
                 </button>
 
                 {/* SideBar */}
-                <div className="sidebar offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div className="sidebar offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     {/* Sidebar Header */}
                     <div className="offcanvas-header text-white border-bottom">
-                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Lampungiana</h5>
+                        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">LAMPCARA</h5>
                         <button type="button" className="btn-close btn-close-white shadow-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
 
@@ -47,31 +71,27 @@ function Navbar(){
                     <div className="sideBody offcanvas-body d-flex flex-column p-4">
                         <ul className="containSide navbar-nav justify-content-center align-item-center fs-5 flex-grow-1">
                             <li className="nav-item mx-2">
-                                <Link className="nav-link" to="/">Home</Link>
+                                <Link className="nav-link" to="/" onClick={closeSidebar}>Home</Link>
                             </li>
-
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Main</a>
-
+                                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Main</a>
                                 <ul className="dropdown dropdown-menu dropdown-menu-dark">
-                                    <li><a className="dropdown-item" href="#mainSatu">Lampung Motto</a></li>
-                                    <li><a className="dropdown-item" href="#mainTiga">Highlight Destination</a></li>
+                                    <li><a className="dropdown-item" href="#mainSatu" onClick={closeSidebar}>Lampung Motto</a></li>
+                                    <li><a className="dropdown-item" href="#mainTiga" onClick={closeSidebar}>Highlight Destination</a></li>
                                 </ul>
                             </li>
-
                             <li className="nav-item mx-2">
-                                <Link className="nav-link" to="/Wisata">Wisata</Link>
+                                <Link className="nav-link" to="/Wisata" onClick={closeSidebar}>Wisata</Link>
                             </li>
                             <li className="nav-item mx-2">
-                                <Link className="nav-link" to="/About">About Us</Link>
+                                <Link className="nav-link" to="/About" onClick={closeSidebar}>About Us</Link>
                             </li>
                             <li className="nav-item mx-2">
-                                <Link className="nav-link" to="/ContactUs">Contact Us</Link>
+                                <Link className="nav-link" to="/ContactUs" onClick={closeSidebar}>Contact Us</Link>
                             </li>
                         </ul>
                     </div>
                 </div>
-                
             </div>
         </nav>
     );
